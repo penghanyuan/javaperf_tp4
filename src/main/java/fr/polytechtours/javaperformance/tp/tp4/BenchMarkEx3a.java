507902ac5019f84f4f4afbdae8d2ffff1c548858
Unknown <penghanyuan@163.com>
2018-11-27 14:44:20 +0100

    add test ex4

--- src/main/java/fr/polytechtours/javaperformance/tp/tp4/BenchMarkEx3a.java
+++ src/main/java/fr/polytechtours/javaperformance/tp/tp4/BenchMarkEx3a.java
@@ -15,10 +15,11 @@ import java.util.concurrent.TimeUnit;
 public class BenchMarkEx3a {
     @Benchmark
     public void test_1(Blackhole blackhole) {
-        int result = 0;
+        Integer result = Integer.valueOf(0);
+        Exercice3a ex3a = new Exercice3a();
         for(int i = 0;i<1;i++){
             try {
-                result = new Exercice3a().exercice3a_1(100, 500000);
+                result = ex3a.exercice3a_1(100, 500000);
             } catch (ExecutionException e) {
                 e.printStackTrace();
             } catch (InterruptedException e) {
@@ -32,10 +33,62 @@ public class BenchMarkEx3a {
 
     @Benchmark
     public void testOrigin(Blackhole blackhole) {
-        int result = 0;
+        Integer result = Integer.valueOf(0);
+        Exercice3a ex3a1 = new Exercice3a();
         for(int i = 0;i<1;i++){
             try {
-                result = new Exercice3a().exercice3a(100, 500000);
+                result = ex3a1.exercice3a(100, 500000);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            blackhole.consume(result);
+        }
+
+    }
+
+    @Benchmark
+    public void test_nolist(Blackhole blackhole) {
+        Integer result = Integer.valueOf(0);
+        Exercice3a ex3a1 = new Exercice3a();
+        for(int i = 0;i<1;i++){
+            try {
+                result = ex3a1.exercice3a_nolistfuture(100, 500000);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            blackhole.consume(result);
+        }
+
+    }
+
+    @Benchmark
+    public void test_int(Blackhole blackhole) {
+        int result = 0;
+        Exercice3a ex3a1 = new Exercice3a();
+        for(int i = 0;i<1;i++){
+            try {
+                result = ex3a1.exercice3a_int(100, 500000);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            blackhole.consume(result);
+        }
+
+    }
+
+    @Benchmark
+    public void test_execute(Blackhole blackhole) {
+        Integer result = Integer.valueOf(0);
+        Exercice3a ex3a1 = new Exercice3a();
+        for(int i = 0;i<1;i++){
+            try {
+                result = ex3a1.exercice3a_excute(100, 500000);
             } catch (ExecutionException e) {
                 e.printStackTrace();
             } catch (InterruptedException e) {
--- src/main/java/fr/polytechtours/javaperformance/tp/tp4/Exercice3a.java
+++ src/main/java/fr/polytechtours/javaperformance/tp/tp4/Exercice3a.java
@@ -16,6 +16,17 @@ public class Exercice3a {
     private static final Object MUTEX = new Object();
 
     private Integer count = 0;
+    private int count_int = 0;
+
+    private synchronized void incrementCounter_int() {
+        count_int++;
+    }
+
+    private void iterate_int(final int nbIteration) {
+        for(int i = 0; i < nbIteration; i++) {
+            this.incrementCounter_int();
+        }
+    }
 
     private synchronized void incrementCounter() {
         synchronized (MUTEX) {
@@ -70,5 +81,49 @@ public class Exercice3a {
         }
         service.shutdown();
         return count;
+    }
+
+    public Integer exercice3a_nolistfuture(final Integer nbThreads, final Integer nbIterationByThread) throws ExecutionException, InterruptedException {
+        final ExecutorService service = Executors.newFixedThreadPool(nbThreads);
+
+        for (Integer i = 0; i < nbThreads; i++) {
+            final Future future = service.submit(() -> iterate_1(nbIterationByThread));
+            future.get();
+        }
+
+        // Wait for it...
+//        for (final Future<Runnable> future : futures) {
+//            future.get();
+//        }
+        service.shutdown();
+        return count;
+    }
+
+    public int exercice3a_int(final int nbThreads, final int nbIterationByThread) throws ExecutionException, InterruptedException {
+        final ExecutorService service = Executors.newFixedThreadPool(nbThreads);
+
+        for (int i = 0; i < nbThreads; i++) {
+            final Future future = service.submit(() -> iterate_int(nbIterationByThread));
+            future.get();
+        }
+        // Wait for it...
+
+        service.shutdown();
+        return count_int;
+    }
+
+    public Integer exercice3a_excute(final Integer nbThreads, final Integer nbIterationByThread) throws ExecutionException, InterruptedException {
+        final ExecutorService service = Executors.newFixedThreadPool(nbThreads);
+
+        for (Integer i = 0; i < nbThreads; i++) {
+            service.execute(()->iterate(nbIterationByThread));
+        }
+
+        service.shutdown();
+        while(true){
+            if(service.isTerminated()){
+                return count;
+            }
+        }
     }
 }
